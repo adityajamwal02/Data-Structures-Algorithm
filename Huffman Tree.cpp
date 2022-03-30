995f934bc8a584338b5d5422dea255066dcb37a7
ADITYA JAMWAL <76657393+adityajamwal02@users.noreply.github.com>
2022-03-30 23:47:56 +0530

    Huffman Coding
    Huffman tree for variable-length encoding (Given Symbol and corresponding Frequency).

---
+++ Huffman Tree.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct MinHeapNode {
+	char data;
+	unsigned freq;
+	MinHeapNode *left, *right;
+	MinHeapNode(char data, unsigned freq)
+	{
+		left = right = NULL;
+		this->data = data;
+		this->freq = freq;
+	}
+};
+
+struct compare {
+	bool operator()(MinHeapNode* l, MinHeapNode* r)
+	{
+		return (l->freq > r->freq);
+	}
+};
+
+void printCodes(struct MinHeapNode* root, string str)
+{
+	if (!root)
+		return;
+	if (root->data != '$')
+		cout << root->data << ": " << str << "\n";
+	printCodes(root->left, str + "0");
+	printCodes(root->right, str + "1");
+}
+
+void HuffmanCodes(char data[], int freq[], int size)
+{
+	struct MinHeapNode *left, *right, *top;
+	priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+	for (int i = 0; i < size; ++i)
+		minHeap.push(new MinHeapNode(data[i], freq[i]));
+	while (minHeap.size() != 1) {
+		left = minHeap.top();
+		minHeap.pop();
+		right = minHeap.top();
+		minHeap.pop();
+		top = new MinHeapNode('$', left->freq + right->freq);
+		top->left = left;
+		top->right = right;
+		minHeap.push(top);
+	}
+	printCodes(minHeap.top(), "");
+}
+int main()
+{
+	char arr[] = { 'Q', 'p', 'T', 'a', 'd' };
+	int freq[] = { 3,23,30,12,18};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	HuffmanCodes(arr, freq, size);
+	return 0;
+}
