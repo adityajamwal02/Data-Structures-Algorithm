4fc506439245789e197f822d9e729b1f31bfe472
ADITYA JAMWAL <76657393+adityajamwal02@users.noreply.github.com>
2022-04-05 18:21:15 +0530

    Total Permuations
    Given an array  of distinct integers/characters, return all the possible permutations in any order.

---
+++ Permutation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void permute(string a, int l, int r)
+{
+    if (l==r)
+        cout<<a<<endl;
+    else
+    {
+        for (int i=l;i<=r;i++)
+        {
+            swap(a[l],a[i]);    //swapping
+            permute(a, l+1, r); //recursion called
+            swap(a[l], a[i]);   //backtrack
+        }
+    }
+}
+int main()
+{
+    string a="1234";
+    int n=a.size();
+    permute(a,0,n-1);
+    return 0;
+}
